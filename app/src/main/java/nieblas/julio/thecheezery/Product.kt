f40ec382a832375a9327d30abe79012a9997d294
JulioNieblas <julionieblasgit@gmail.com>
2023-02-24 23:40:19 -0600

    commit con listviews

---
+++ app/src/main/java/nieblas/julio/thecheezery/Product.kt
@@ -0,0 +1,5 @@
+package nieblas.julio.thecheezery
+data class Product (var name:String,
+                    var image:Int,
+                    var description:String,
+                    var price:Double)
